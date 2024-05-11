2abbc83f4454ac8d2844d43f9efdbccf28ac7edb
lsj2015-hub <lsj2015@hotmail.com>
2024-05-11 19:59:17 +0800

    setting Drizzle ORM and Neon Database

---
+++ drizzle.config.ts
@@ -0,0 +1,25 @@
+import { config } from 'dotenv';
+import { defineConfig } from 'drizzle-kit';
+
+config({ path: '.env.local' });
+
+export default defineConfig({
+  schema: './db/schema.ts',
+  driver: 'pg',
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL!,
+  },
+  verbose: true,
+  strict: true,
+});
+
+// import { defineConfig } from 'drizzle-kit';
+// export default defineConfig({
+//   schema: './db/schema.ts',
+//   dialect: 'postgresql',
+//   dbCredentials: {
+//     url: process.env.DATABASE_URL!,
+//   },
+//   verbose: true,
+//   strict: true,
+// });
