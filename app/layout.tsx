388aab61db83f1e4fa06592e2e2e8bf9c1984950
lsj2015-hub <lsj2015@hotmail.com>
2024-05-12 14:45:46 +0800

    Account Post API by using react-query, shacdn form, sonner, zValidator

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
+
 import './globals.css';
 import { QueryProviders } from '@/providers/query-provider';
+import { SheetProvider } from '@/providers/sheet-provider';
+import { Toaster } from 'sonner';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,7 +23,11 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <QueryProviders>{children}</QueryProviders>
+          <QueryProviders>
+            <SheetProvider />
+            <Toaster />
+            {children}
+          </QueryProviders>
         </body>
       </html>
     </ClerkProvider>
