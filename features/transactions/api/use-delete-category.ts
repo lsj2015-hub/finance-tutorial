e9b4774eb00f48339b27a2db4bdaa97086e098b0
lsj2015-hub <lsj2015@hotmail.com>
2024-05-14 18:32:45 +0800

    Transaction API

---
+++ features/transactions/api/use-delete-category.ts
@@ -0,0 +1,33 @@
+import { InferResponseType } from 'hono';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { client } from '@/lib/hono';
+import { toast } from 'sonner';
+
+type ResponseType = InferResponseType<
+  (typeof client.api.categories)[':id']['$delete']
+>;
+
+export const useDeleteCategory = (id?: string) => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error>({
+    mutationFn: async () => {
+      const response = await client.api.categories[':id']['$delete']({
+        param: { id },
+      });
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success('Category deleted');
+      queryClient.invalidateQueries({ queryKey: ['catrgory', { id }] });
+      queryClient.invalidateQueries({ queryKey: ['categories'] });
+      // TODO: Also invalidate summary and transactions
+    },
+    onError: () => {
+      toast.error('Failed to delete catrgory');
+    },
+  });
+
+  return mutation;
+};
